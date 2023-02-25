2a37363653bb230c6c852066bfd550319bda0d51
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-02-25 13:19:28 -0300

    🔥 feat: set up react router dom

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../../assets/styles/GlobalStyles';
 import darkTheme from '../../assets/styles/themes/darkTheme';
+import Router from '../../routes';
 
 export default function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyles />
-      <h1>App</h1>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyles />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
